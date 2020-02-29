b7d0850eae3eff16d7f960838ac2a23b78424c4e
Nazan ozcelik <nazanozcelik7@gmail.com>
2020-02-29 19:56:39 +0300

    Login Page eklendi

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { Home } from './components/Home';
 import { About } from './components/About';
 import { NoMatch } from './components/NoMatch';
+import Login from './components/Login';
 import Sidebar from './components/Sidebar';
  class App extends Component {
   render() {
@@ -22,6 +23,7 @@ import Sidebar from './components/Sidebar';
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/registration" component={Registration}/>
+          <Route path = "/login" component = {Login}/>
           <Route component={NoMatch} />
         </Switch>
       </Router>
---
+++ src/components/Login.js
@@ -0,0 +1,36 @@
+import React,{Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class Login extends Component {
+    render() {
+        return (
+            <div className="container">
+                <br/>
+                <div className="card">
+                    <div className="card-body">
+                    <h2 className="card-title">Sign in</h2>
+                    <br/>
+                    <div className="row">
+                        <div className="col-sm">
+                        <form>
+                            <div className="form-group">
+                            <label for="email">E-mail or Username</label>
+                            <input type="text" className="form-control" id="email"/>
+                            </div>
+                            <div className="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1"/>
+                            </div>
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                        </form>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            
+        );
+    }
+}
+
+export default Login;
