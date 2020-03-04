4601af963fe9fffbdc9efc080a8d9aa5e44de5fe
Nazan ozcelik <nazanozcelik7@gmail.com>
2020-03-04 13:27:06 +0300

    Login Page düzenlendi

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,20 +6,20 @@ class Login extends Component {
         return (
             <div className="container">
                 <br/>
-                <div className="card">
+                <div className="card center" style={{width:400}}>
                     <div className="card-body">
                     <h2 className="card-title">Sign in</h2>
                     <br/>
                     <div className="row">
                         <div className="col-sm">
                         <form>
-                            <div className="form-group">
+                            <div className="form-group " >
                             <label for="email">E-mail or Username</label>
-                            <input type="text" className="form-control" id="email"/>
+                            <input type="text"  className="form-control" id="email" style ={{ width:250}}/>
                             </div>
-                            <div className="form-group">
+                            <div className="form-group ">
                             <label for="exampleInputPassword1">Password</label>
-                            <input type="password" className="form-control" id="exampleInputPassword1"/>
+                            <input type="password" className="form-control" id="exampleInputPassword1" style ={{ width:250}}/>
                             </div>
                             <button type="submit" className="btn btn-primary">Submit</button>
                         </form>
