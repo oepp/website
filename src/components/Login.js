ec9abb6ba3093ff9ca102cf6ab5ff96ffcb2f8b4
Nazan ozcelik <nazanozcelik7@gmail.com>
2020-03-04 14:25:57 +0300

    Login Page güncellendi.

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,24 +4,24 @@ import 'bootstrap/dist/css/bootstrap.css';
 class Login extends Component {
     render() {
         return (
-            <div className="container">
+            <div className="container" style ={{paddingLeft:200}}>
                 <br/>
                 <div className="card center" style={{width:400}}>
-                    <div className="card-body">
-                    <h2 className="card-title">Sign in</h2>
+                    <div className="card-body" style ={{padding:40}}>
+                    <h2 className="card-title"style={{fontSize:20}} style ={{textAlign:"center"}} style={{paddingBlockStart:20}}style ={{paddingLeft:100}}>Sign in</h2>
                     <br/>
                     <div className="row">
                         <div className="col-sm">
                         <form>
                             <div className="form-group " >
-                            <label for="email">E-mail or Username</label>
+                            <label for="email" >E-mail or Username</label>
                             <input type="text"  className="form-control" id="email" style ={{ width:250}}/>
                             </div>
-                            <div className="form-group ">
+                            <div className="form-group " >
                             <label for="exampleInputPassword1">Password</label>
                             <input type="password" className="form-control" id="exampleInputPassword1" style ={{ width:250}}/>
                             </div>
-                            <button type="submit" className="btn btn-primary">Submit</button>
+                            <button type="submit" className="btn btn-primary" style ={{backgroundColor:"purple"}}>Submit</button>
                         </form>
                         </div>
                     </div>
