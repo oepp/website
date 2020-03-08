7e701bbb2ed6d5b34b569cfa2710d693cc7ecb64
AlpYksl <56409177+AlpYksl@users.noreply.github.com>
2020-03-08 22:10:58 +0300

    Home Page eklendi
    Home Page eklendi

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
-import Card from './UserContext/Card'
-
+import Cards from './UserContext/Cards';
 
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                
-                <Card/>
+            <div>      
+                <h1 style={{float:'left'}}> Newest Game in our Storage</h1>
+                <div className="btn-group-vertical col-md-8 align-right" style={{float:'right'}}>
+                <button className="btn btn-primary">Make Your Own Game</button>
+                <button className="btn btn-danger">Update Your Existing Game</button>
+                </div>
+                <Cards/>
             </div>
         )
     }
