4cf8620411030491fd959baba89b43a3a43bf160
Nazan ozcelik <nazanozcelik7@gmail.com>
2020-04-12 01:18:29 +0300

    Implemented Password Reset Logic

--- src/components/ConfirmPassword.js
+++ src/components/ConfirmPassword.js
@@ -2,6 +2,64 @@ import React,{Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 
 class ConfirmPassword extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            submitting: false,
+            password: "",
+            confirmpassword:"",
+            passwordResetCode:""
+
+        }
+    }
+
+    setPassword(event){
+        this.setState({
+            password: event.target.value
+        })
+    }
+    
+    setConfirmPassword(event){
+        this.setState({
+            confirmpassword: event.target.value
+        })
+    }
+    setResetPasswordCode(event){
+        this.setState({
+            passwordResetCode: event.target.value
+        })
+    }
+
+    submitClick(event){
+        event.preventDefault();
+        this.setState({
+            submitting: true
+        });
+        const params = {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                password: this.state.password,
+                confirmpassword: this.state.confirmpassword,
+                passwordResetCode: this.state.passwordResetCode,
+            })
+        }
+        fetch("http://localhost:3001/user/forgot/confirm/password", params)
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+                if(result.status === "success"){
+                    alert("Password updated succesfully!");
+                    window.location.replace("/ConfirmPassword");
+                }
+                if(result.status === "error"){
+                    alert(result.message);
+                }
+                this.setState({
+                    submitting: false
+                });
+            });
+    }
     render() {
         return (
             <div className="container" style={{paddingLeft:250}}>
@@ -14,25 +72,30 @@ class ConfirmPassword extends Component{
                             <div className="form-row" style={{paddingLeft:150,fontSize:13,fontWeight:'normal'}}>
                             <div className="col-8" >
                             <label for="exampleInputPassword1" >Verification Code</label>
-                            <input className="form-control form-control-lg" type="text" placeholder="Verification Code"></input>
+                            <input className="form-control form-control-lg" type="text" placeholder="Verification Code" onChange = {this.setResetPasswordCode.bind(this)}></input>
                             </div> &nbsp; &nbsp; 
                             </div><br/>
                             <div className="form-row" style={{paddingLeft:150,fontSize:13,fontWeight:"lighter"}}>
                             <div className="col-8" >
                             <label for="exampleInputPassword1"> New Password</label>
-                            <input className="form-control form-control-lg" type="text" placeholder="New Password"></input>
+                            <input className="form-control form-control-lg" type="password" placeholder="New Password" onChange = {this.setPassword.bind(this)}></input>
                             </div>  &nbsp; &nbsp; 
                             </div><br/>
                             <div className="form-row" style={{paddingLeft:150,fontSize:13,fontWeight:'normal'}}>
                             <div className="col-8" >
                             <label for="exampleInputPassword1"> Confirm Password</label>
-                            <input className="form-control form-control-lg" type="password" placeholder="Confirm Password"></input>
+                            <input className="form-control form-control-lg" type="password" placeholder="Confirm Password" onChange = {this.setConfirmPassword.bind(this)}></input>
                             </div>  &nbsp; &nbsp; 
                             </div><br/>
                             &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
                             <div style={{paddingLeft:250}}>
-                            <button type="button" className="btn btn-primary btn-lg" style={{backgroundColor:'#5062e1'}}>&nbsp; &nbsp;Submit&nbsp; &nbsp;</button>
-                            </div>
+                            <button type="button" className="btn btn-primary btn-lg" style={{backgroundColor:'#5062e1'}} onClick={this.submitClick.bind(this)}>
+                                {this.state.submitting === true ?
+                                "Submitting..."
+                                :
+                                "Submit"
+                                }
+                            </button>                            </div>
                         </div>
                     </div>
                     </div>
--- src/components/ForgotAccount.js
+++ src/components/ForgotAccount.js
@@ -2,6 +2,49 @@ import React,{Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 
 class ForgotAccount extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            submitting: false,
+            email: "",
+        }
+    }
+
+    setEmail(event){
+        this.setState({
+            email: event.target.value
+        })
+    }
+
+    submitClick(event){
+        event.preventDefault();
+        this.setState({
+            submitting: true
+        });
+        const params = {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                email: this.state.email,
+            })
+        }
+        fetch("http://localhost:3001/user/forgot/password", params)
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+                if(result.status === "success"){
+                    alert("Code Sent! Check your email!");
+                    window.location.replace("/ConfirmPassword");
+                }
+                if(result.status === "error"){
+                    alert(result.message);
+                }
+                this.setState({
+                    submitting: false
+                });
+            });
+    }
+    
     render() {
         return (
             <div className="container" style={{paddingLeft:250}}>
@@ -13,11 +56,17 @@ class ForgotAccount extends Component{
                         <div className="col-sm">
                             <div className="form-row">
                             <div className="col-6" style={{paddingLeft:75}}>
-                            <input className="form-control form-control-lg" type="text" placeholder="Email"></input>
+                            <input className="form-control form-control-lg" type="text" placeholder="Email" id="email" onChange = {this.setEmail.bind(this)}></input>
                             </div> 
                             <div className="col-5" style={{paddingLeft:50}}>
-                            <button type="button" className="btn btn-primary btn-lg" style={{backgroundColor:'#5062e1'}}>&nbsp; &nbsp;Send Verification Code&nbsp; &nbsp;</button>
-                            </div>  &nbsp; &nbsp; 
+                            <button type="button" className="btn btn-primary btn-lg" style={{backgroundColor:'#5062e1'}} onClick={this.submitClick.bind(this)}>
+                                {this.state.submitting === true ?
+                                "Submitting..."
+                                :
+                                "Send Verification Code"
+                                }
+                            </button>
+                            </div>  &nbsp; &nbsp;
                             </div><br/>
                         </div>
                     </div>
