f31d3bbf49ba3f70a1d542c2906ca8ff53db60cc
Nedret Çelik <nedretcelik97@gmail.com>
2020-04-13 21:48:19 +0300

    Added Logo in Registration and Login Pages.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React,{Component} from 'react';
+import React,{Component, Fragment} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 
 class Login extends Component {
@@ -55,37 +55,40 @@ class Login extends Component {
     }
     render() {
         return (
-            <div className="container" style ={{paddingLeft:275}}>
+            <div className="container" style={{paddingLeft:150}}>
                 <br/>
-                <div className="card center" style={{width:500, marginTop:100 , height:300}}>
+                <div className="card center"style={{width:850,height:400,  marginTop:100}}>
                     <div className="card-body" style={{backgroundColor:'#fafafa'}}>
-                    <center><h1 className="card-title">Sign in</h1></center>
-                    <br/>
-                    <div className="row">
-                        <div className="col-sm">
-                        <form style={{paddingLeft:80, paddingRight:50, width:700}}>
-                            <div className="form-group">
-                            <label for="email"  style={{fontSize:15}}>Username</label>
-                            <input type="text"  className="form-control" id="email" onChange = {this.setUsername.bind(this)} style ={{ width:300}}/>
+                        <center><h1 className="card-title">Sign in</h1></center>
+                        <br/>
+                        <div className="row" style={{paddingRight:250}}>
+                            <div className="col-sm">
+                                <form  style={{paddingLeft:80}}>
+                                    <div className="form-group">
+                                    <label for="email"  style={{fontSize:15}}>Username</label>
+                                    <input type="text"  className="form-control" id="email" onChange = {this.setUsername.bind(this)} style ={{ width:300}}/>
+                                    </div>
+                                    <div className="form-group">
+                                    <label for="exampleInputPassword1"  style={{fontSize:15}}>Password</label>
+                                    <input type="password" className="form-control" id="exampleInputPassword1" onChange = {this.setPassword.bind(this)} style ={{ width:300}}/>
+                                    </div>
+                                    <div>
+                                        <h5 style={{paddingLeft:170}}><a href="./ForgotAccount" style={{backgroundColor:'#fef3d0', color:'#001fff'}}>Forgot your account?</a></h5>
+                                    </div>
+                                    {this.state.submitting === true ?
+                                    <button className="btn btn-info" style={{backgroundColor:"#17a2b8" ,fontSize:16}} type="button" disabled>
+                                    <span className="spinner-border spinner-border-md" role="status" aria-hidden="true"></span>
+                                            &nbsp;Please Wait..
+                                    </button>
+                                        : 
+                                        <button type="submit" onClick={this.submitClick.bind(this)} className="btn btn-primary" style={{backgroundColor:"#17a2b8" ,fontSize:16}} >Login</button>
+                                        }             
+                                </form>
                             </div>
-                            <div className="form-group">
-                            <label for="exampleInputPassword1"  style={{fontSize:15}}>Password</label>
-                            <input type="password" className="form-control" id="exampleInputPassword1" onChange = {this.setPassword.bind(this)} style ={{ width:300}}/>
-                            </div>
-                            <div>
-                                <h5 style={{paddingLeft:170}}><a href="./ForgotAccount" style={{backgroundColor:'#fef3d0', color:'#001fff'}}>Forgot your account?</a></h5>
-                            </div>
-                            {this.state.submitting === true ?
-                            <button className="btn btn-info" style={{backgroundColor:"#17a2b8" ,fontSize:16}} type="button" disabled>
-                            <span className="spinner-border spinner-border-md" role="status" aria-hidden="true"></span>
-                                    &nbsp;Please Wait..
-                          </button>
-                            : 
-                             <button type="submit" onClick={this.submitClick.bind(this)} className="btn btn-primary" style={{backgroundColor:"#17a2b8" ,fontSize:16}} >Login</button>
-                            }             
-                         </form>
+                            <Fragment>
+                                    <img style={{width:200, height:200}} src="/img/logo_transparent.png"/>
+                            </Fragment>
                         </div>
-                    </div>
                     </div>
                 </div>
             </div>
