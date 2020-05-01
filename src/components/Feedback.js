bb7d0e8647dea5d505ccba74797033f70ba2b596
Nazan ozcelik <nazanozcelik7@gmail.com>
2020-05-01 20:31:17 +0300

    Feedback and Faq Page Updated.

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -26,16 +26,16 @@ class Feedback extends Component {
                 <br/>
                 <div className="card center"  style={{width:600}} >
                     <div className="card-body" style={{backgroundColor:'#fafafa'}}>
-                    <center><h1 className="card-title">Feedback</h1></center>
+                    <center><h1 className="card-title" style={{fontSize:35}}>FeedBack</h1></center>
                     <div className="row">
                         <div className="col-sm">
                         <form style={{paddingLeft:30, paddingRight:30, width:500}}>
                             <div className="form-group">
-                                <label for="courseName" style={{fontSize:13}}>Course Name</label>
+                                <label for="courseName" style={{fontSize:16}}>Course Name</label>
                                 <input type="email" className="form-control" id="feedbacourseNameckEmail"/>
                             </div>
-                            <div className="form-group">
-                                <label for="message" style={{fontSize:13}}>Message</label>
+                            <div className="form-group" >
+                                <label for="message" style={{fontSize:16}}>Message</label>
                                 <textarea className="form-control" id="message" rows="10" onChange = {this.setMessage.bind(this)}></textarea>
                             </div>
                             {this.state.submitting === true ?
@@ -44,7 +44,7 @@ class Feedback extends Component {
                                     &nbsp;Please Wait..
                           </button>
                             : 
-                             <button type="submit" onClick={this.submitClick.bind(this)} className="btn btn-primary" style={{backgroundColor:"#17a2b8" ,fontSize:16}} >Send</button>
+                             <button type="submit" onClick={this.submitClick.bind(this)} className="btn btn-primary" style={{backgroundColor:"#17a2b8" ,fontSize:16, marginleft:30}} >Send</button>
                             }                                 
                             </form>
                         </div>
